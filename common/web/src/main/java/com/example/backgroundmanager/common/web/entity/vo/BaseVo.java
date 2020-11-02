7f10768bd544df7d770037ffa67f403780dfb8f0
huangzd <1096229481@qq.com>
2020-11-02 23:56:34 +0800

    公用部分代码提交

---
+++ common/web/src/main/java/com/example/backgroundmanager/common/web/entity/vo/BaseVo.java
@@ -0,0 +1,13 @@
+package com.example.backgroundmanager.common.web.entity.vo;
+
+import com.springboot.cloud.common.web.entity.po.BasePo;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class BaseVo<T extends BasePo> implements Serializable {
+    private String id;
+}
