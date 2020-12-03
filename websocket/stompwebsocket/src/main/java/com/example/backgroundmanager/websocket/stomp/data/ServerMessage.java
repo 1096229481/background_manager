eeb8fd831fffad2a04584e5fbd096ec348c2f8bc
huangzd <1096229481@qq.com>
2020-12-03 20:49:46 +0800

    提交

---
+++ websocket/stompwebsocket/src/main/java/com/example/backgroundmanager/websocket/stomp/data/ServerMessage.java
@@ -0,0 +1,20 @@
+package com.example.backgroundmanager.websocket.stomp.data;
+
+/**
+ * @author as huangzd
+ */
+public class ServerMessage {
+    private String responseMessage;
+
+    public ServerMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
